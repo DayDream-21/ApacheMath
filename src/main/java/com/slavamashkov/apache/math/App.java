2b9b882d49fe77bd83dfebd609a1111dad772f8a
DayDream-21 <vova13158@gmail.com>
2021-09-25 22:06:03 +0300

    Initial commit

---
+++ src/main/java/com/slavamashkov/apache/math/App.java
@@ -0,0 +1,13 @@
+package com.slavamashkov.apache.math;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
